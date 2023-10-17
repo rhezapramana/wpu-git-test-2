d19fae25b08a4627a8d99267667cce5649225412
rhezapramana <rhezapramanadahlan@gmail.com>
2023-10-17 16:16:13 +0700

    menambahkan paragraf baru

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba git remote</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia dolorem recusandae quos tempore? Corporis similique nulla expedita repellat velit officia minus deleniti. At delectus facere odio rerum saepe nostrum labore.</p>
+</body>
+</html>
